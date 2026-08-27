1be70529b404c3ee8332e45b5a4f53f0f282ad3b
Carlos Aparicio <carlosapariciodes@gmail.com>
2026-08-27 10:37:45 +0200

    feat: add HTTPSig placeholder in internal/common

---
+++ internal/common/httpsig.go
@@ -0,0 +1,10 @@
+// Package common holds helpers shared across the application.
+package common
+
+// HTTPSig builds HTTP message signatures.
+type HTTPSig struct{}
+
+// Build assembles the signature. It is not implemented yet.
+func (s *HTTPSig) Build() error {
+	return nil
+}
